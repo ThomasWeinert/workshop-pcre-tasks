e260cbfbc7a346ee0adbd375e5570257e66695d5
Thomas Weinert <thomas@weinert.info>
2017-02-25 16:00:40 +0100

    Validate string starts with digit

--- task.php
+++ task.php
@@ -1,15 +1,14 @@
 <?php
 
 /*
-Validate that the subject is a German zip code.
-German zip codes consist of 5 digits.
+Validate that the subject start with a digit.
 */
-$pattern = '(^\\d)';
+$pattern = '()';
 
 /* DO NOT CHANGE */
 $subjects = [
   '1. match' => TRUE, '2. match' => TRUE, '42' => TRUE,
-  'not a match' => FALSE, "number at the end 2" => FALSE
+  'not a match' => FALSE, "in 345 between" => FALSE,  "at the end 2" => FALSE
 ];
 foreach ($subjects as $subject => $shouldMatch) {
   if ($shouldMatch == preg_match($pattern, $subject)) {
