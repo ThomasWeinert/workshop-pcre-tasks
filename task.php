18718e31016d871863110834aa9de9d4525e266c
Thomas Weinert <thomas@weinert.info>
2017-02-25 13:29:58 +0100

    3 different variants in $subject

--- task.php
+++ task.php
@@ -5,7 +5,7 @@ $pattern = '()';
 
 /* DO NOT CHANGE */
 $result = preg_match_all(
-  $pattern, 'code CODE code', $matches
+  $pattern, 'code CODE Code', $matches
 );
 if ($result && count($matches[0]) == 3) {
   echo 'SUCCESS';
