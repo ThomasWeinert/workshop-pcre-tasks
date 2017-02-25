fa60829aeb265a1b9e7a897cef56343bdb9853fd
Thomas Weinert <thomas@weinert.info>
2017-02-25 22:54:14 +0100

    Validate language code

--- task.php
+++ task.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
-Validate that the input is an integer.
-The leading sign is optional.
+Validate that the input is an ISO639-1 or ISO639-2 language code.
+The are strings 2 or 3 letters.
 */
 $pattern = '()';
 
 $subjects = [
-  '1' => TRUE, '123' => TRUE,
-  '+123' => TRUE, '-456' => TRUE,
-  '1.1' => FALSE, "abc" => FALSE, "123 456" => FALSE
+  'en' => TRUE, 'de' => TRUE,
+  'eng' => TRUE, 'deu' => TRUE,
+  '123' => FALSE, "en-US" => FALSE
 ];
 foreach ($subjects as $subject => $shouldMatch) {
   if ($shouldMatch == preg_match($pattern, $subject)) {
