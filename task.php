9fdce0d9ed513ec475114d2aaf5fbc3ecf4197c7
Thomas Weinert <thomas@weinert.info>
2017-02-25 13:50:02 +0100

    Match anything but newline (using the .)

--- task.php
+++ task.php
@@ -5,9 +5,9 @@ $pattern = '()';
 
 /* DO NOT CHANGE */
 $result = preg_match_all(
-  $pattern, 'code CODE Code', $matches
+  $pattern, "ab.cd.ef.gh\na\n.b\n.d\n.e\n\nabcdefghiklm", $matches
 );
-if ($result && count($matches[0]) == 3) {
+if ($result && count($matches[0]) == 1) {
   echo 'SUCCESS';
 } else {
   echo 'FAIL';
