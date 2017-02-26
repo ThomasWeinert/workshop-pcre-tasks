cdda1da1a65ba5440d0872f908df466e2e8c42ca
Thomas Weinert <thomas@weinert.info>
2017-02-26 14:21:08 +0100

    minor formatting fixes

--- task.php
+++ task.php
@@ -1,14 +1,14 @@
 <?php
 
 /*
-Validate that the subject start with a digit.
+Validate that the subject starta with a digit.
 */
 $pattern = '()';
 
 /* DO NOT CHANGE */
 $subjects = [
   '1. match' => TRUE, '2. match' => TRUE, '42' => TRUE,
-  'not a match' => FALSE, "a 345 b" => FALSE,  "end 3" => FALSE
+  'no match' => FALSE, "a 345 b" => FALSE, "end 3" => FALSE
 ];
 foreach ($subjects as $subject => $shouldMatch) {
   if ($shouldMatch == preg_match($pattern, $subject)) {
