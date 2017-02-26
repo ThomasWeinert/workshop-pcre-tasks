885bd5c57f7a0d6c3a6e0cce800c50cf4c041394
Thomas Weinert <thomas@weinert.info>
2017-02-26 12:35:04 +0100

    Match unicode letters

--- task.php
+++ task.php
@@ -1,11 +1,12 @@
 <?php
 
 /* Define the pattern argument */
-$pattern = '';
+$pattern = '(\pL)u';
 
-/* DO NOT CHANGE */
-$result = preg_match_all($pattern, 'https://nevercodealone.de', $matches);
-if ($result && count($matches[0]) == 1) {
+$result = preg_match_all(
+  $pattern, 'English, Русский, 中文', $matches
+);
+if ($result && count($matches[0]) == 16) {
   echo 'SUCCESS';
 } else {
   echo 'FAIL';
