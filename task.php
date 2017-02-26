029932d8b356818883c90fbfd5155fec359d90b7
Thomas Weinert <thomas@weinert.info>
2017-02-26 14:24:40 +0100

    minor formatting fixes

--- task.php
+++ task.php
@@ -8,7 +8,7 @@ $pattern = '()';
 /* DO NOT CHANGE */
 $subjects = [
   'match 1' => TRUE, 'match 2' => TRUE, '42' => TRUE, "21\n" => TRUE,
-  'not a match' => FALSE, "a 345 b" => FALSE,  "3 start" => FALSE
+  'no match' => FALSE, "a 345 b" => FALSE, "3 start" => FALSE
 ];
 foreach ($subjects as $subject => $shouldMatch) {
   if ($shouldMatch == preg_match($pattern, $subject)) {
