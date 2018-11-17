2da890219c3b8c213558be1e33342c2d55acf7a8
Thomas Weinert <thomas@weinert.info>
2018-11-17 11:57:45 +0100

    Fix inspections

--- task.php
+++ task.php
@@ -8,10 +8,10 @@ $pattern = '()';
 /* DO NOT CHANGE */
 $subjects = [
   '1. match' => TRUE, '2. match' => TRUE, '42' => TRUE,
-  'no match' => FALSE, "a 345 b" => FALSE, "end 3" => FALSE
+  'no match' => FALSE, 'a 345 b' => FALSE, 'end 3' => FALSE
 ];
 foreach ($subjects as $subject => $shouldMatch) {
-  if ($shouldMatch == preg_match($pattern, $subject)) {
+  if ($shouldMatch === (bool)\preg_match($pattern, $subject)) {
     echo "SUCCESS\n";
   } else {
     echo "FAIL\n";
