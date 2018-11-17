89dcb75f5da66e3ec0471cce7a88440bd55983a7
Thomas Weinert <thomas@weinert.info>
2018-11-17 11:55:45 +0100

    Fix inspections

--- task.php
+++ task.php
@@ -9,10 +9,10 @@ $pattern = '()';
 $subjects = [
   '127.0.0.1' => TRUE, '0.0.0.0' => TRUE,
   '255.255.255.0' => TRUE, '1.1.1.256' => FALSE,
-  "1.1.1.a" => FALSE, "-1.1.1.1" => FALSE
+  '1.1.1.a' => FALSE, '-1.1.1.1' => FALSE
 ];
 foreach ($subjects as $subject => $shouldMatch) {
-  if ($shouldMatch == preg_match($pattern, $subject)) {
+  if ($shouldMatch === (bool)\preg_match($pattern, $subject)) {
     echo "SUCCESS\n";
   } else {
     echo "FAIL\n";
