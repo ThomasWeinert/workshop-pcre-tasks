977de5a4ede89ac8ca371ed0d973065686be24ec
Thomas Weinert <thomas@weinert.info>
2018-11-17 11:34:21 +0100

    Fix inspections

--- task.php
+++ task.php
@@ -6,7 +6,7 @@ $pattern = '()';
 $result = preg_match_all(
   $pattern, 'English, Русский, 中文', $matches
 );
-if ($result && count($matches[0]) == 16) {
+if ($result && \count($matches[0]) === 16) {
   echo 'SUCCESS';
 } else {
   echo 'FAIL';
