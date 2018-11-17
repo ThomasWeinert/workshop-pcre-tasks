88a5c917ceb28deb35342634b978f680b8158dfb
Thomas Weinert <thomas@weinert.info>
2018-11-17 11:26:16 +0100

    Fix inspections

--- task.php
+++ task.php
@@ -7,7 +7,7 @@ $pattern = '()';
 $result = preg_match_all(
   $pattern, 'code CODE Code', $matches
 );
-if ($result && count($matches[0]) == 3) {
+if ($result && \count($matches[0]) === 3) {
   echo 'SUCCESS';
 } else {
   echo 'FAIL';
