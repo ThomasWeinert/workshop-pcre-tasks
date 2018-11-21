84cc39316b2397815f1520cafa8d95d4e259b931
Thomas Weinert <thomas@weinert.info>
2018-11-21 20:46:54 +0100

    Root namespace

--- task.php
+++ task.php
@@ -7,7 +7,7 @@ x representing a digit, X a non-digit.
 $pattern = '()';
 
 /* DO NOT CHANGE */
-$result = preg_match_all(
+$result = \preg_match_all(
   $pattern, "12.34.5678\n123456789\nab.cd.efgh", $matches
 );
 if ($result && \count($matches[0]) === 1) {
