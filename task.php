7a7ddb6b6978858bd32a864a54b9e56d9b768217
Thomas Weinert <thomas@weinert.info>
2018-11-21 20:48:12 +0100

    Better task description

--- task.php
+++ task.php
@@ -1,10 +1,10 @@
 <?php
 
-/* Define the pattern argument */
+/* Match any character that is not a vowel (aeoui) */
 $pattern = '()';
 
 /* DO NOT CHANGE */
-$result = preg_match_all($pattern, 'https://nevercodealone.de', $matches);
+$result = \preg_match_all($pattern, 'https://nevercodealone.de', $matches);
 if ($result && \count($matches[0]) === 17) {
   echo 'SUCCESS';
 } else {
