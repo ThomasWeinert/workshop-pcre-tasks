be66ea06ef596b2f6f341f12983e2f1df8852044
Thomas Weinert <thomas@weinert.info>
2018-11-21 20:44:44 +0100

    Better task description

--- task.php
+++ task.php
@@ -1,10 +1,10 @@
 <?php
 
-/* Define the pattern argument */
+/* Match a string in the patter "cc.cc.cc.cc" (four groups of two characters separated by dots) */
 $pattern = '()';
 
 /* DO NOT CHANGE */
-$result = preg_match_all(
+$result = \preg_match_all(
   $pattern, "ab.cd.ef.gh\na\n.b\n.d\n.e\n\nabcdefghiklm", $matches
 );
 if ($result && \count($matches[0]) === 1) {
