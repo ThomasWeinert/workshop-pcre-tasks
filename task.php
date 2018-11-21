0bed3d557f4479c29e674c51b2f627aa569b6cea
Thomas Weinert <thomas@weinert.info>
2018-11-21 20:34:50 +0100

    Better comment, task description

--- task.php
+++ task.php
@@ -4,7 +4,7 @@
 $pattern = '';
 
 /* DO NOT CHANGE */
-$result = preg_match_all($pattern, 'https://nevercodealone.de', $matches);
+$result = \preg_match_all($pattern, 'https://nevercodealone.de', $matches);
 if ($result && \count($matches[0]) === 1) {
   echo 'SUCCESS';
 } else {
