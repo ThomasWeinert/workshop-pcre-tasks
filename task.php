3a43a725bbbc94ca58440b56c79327622e723580
Thomas Weinert <thomas@weinert.info>
2018-11-21 20:49:19 +0100

    Better task description

--- task.php
+++ task.php
@@ -1,9 +1,9 @@
 <?php
 
-/* Define the pattern argument */
-$pattern = '()';
+/* Match all unicode letters. */
+$pattern = '()u';
 
-$result = preg_match_all(
+$result = \preg_match_all(
   $pattern, 'English, Русский, 中文', $matches
 );
 if ($result && \count($matches[0]) === 16) {
