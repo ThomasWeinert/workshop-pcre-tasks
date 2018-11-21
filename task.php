056bd738bb4e20759da27a09c415c7e9048213d6
Thomas Weinert <thomas@weinert.info>
2018-11-21 20:50:32 +0100

    Better task description

--- task.php
+++ task.php
@@ -1,11 +1,11 @@
 <?php
 
-/* Define the pattern argument */
+/* Match all vowels (aeiou). */
 $pattern = '()';
 
 /* DO NOT CHANGE */
-$result = preg_match_all($pattern, 'https://nevercodealone.de', $matches);
-if ($result && count($matches[0]) == 8) {
+$result = \preg_match_all($pattern, 'https://nevercodealone.de', $matches);
+if ($result && \count($matches[0]) === 8) {
   echo 'SUCCESS';
 } else {
   echo 'FAIL';
